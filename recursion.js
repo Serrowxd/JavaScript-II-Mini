f5acae64cb65c662da7d50eeea062a69a37cb77a
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-02-22 14:44:46 -0600

    [Complete] JS-II Mini

--- recursion.js
+++ recursion.js
@@ -11,6 +11,21 @@ while (n <= 10) {
 
 // code here
 
+const countToTen = n => {
+  if (n >= 10) {
+    return n++;
+  }
+  else if (n == 0) {
+    return 1;
+  } else {
+    console.log(n);
+    return (countToTen(n + 1));
+  }
+};
+console.log(countToTen(1));
+
+// Harrison Crawford
+
 // when you code is ready, un-comment the next line and run the file
 // console.log(countToTen());
 /* ================ Next Problem ================= */
@@ -29,5 +44,16 @@ console.log(factorial(5));
 
 // write the above function in a recursive way.
 
+const recursiveFactorial = n => {
+  if (n < 0) {
+    return;
+  }
+  else if (n == 0) {
+    return 1;
+  } else {
+    return (n * recursiveFactorial(n -1));
+  }
+}
+
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(5));
